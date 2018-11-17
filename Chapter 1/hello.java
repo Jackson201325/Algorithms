8d1793c89c92182251b7cbe075d63bb5bb98dd8d
Jackson Huang <jacksonh201325@gmail.com>
2018-11-17 14:07:42 -0300

    First commit 11/17/18

---
+++ Chapter 1/hello.java
@@ -0,0 +1,13 @@
+public class hello
+{
+    public static void main(String[] args)
+    {
+        double[] a;
+        a = new double[9];
+        for (int i = 0; i < 10; i++)
+        {
+            a[i] = 0.0;
+        }
+
+    }
+}
